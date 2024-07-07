3ea9351f36911ee08a62734ec3e5643558c4f3e3
Vukosi <vukosimohlabini16@gmail.com>
2024-07-07 20:17:53 +0000

    Done :)

---
+++ app.js
@@ -0,0 +1,31 @@
+const myLibrary = [];
+
+function Book(authour, name, year, publisher){
+    this.authour = authour;
+    this.name = name;
+    this.year = year;
+    this.publisher = publisher;
+
+}
+
+function getBookInfo(){
+    const bookName = prompt("Enter the name of the book: ");
+    const bookAuthor = prompt("Enter the author of the book: ");
+    const bookYear = prompt("Enter the year the book got published.");
+    const bookPublisher = prompt("Enter the name of the publisher");
+
+    return {bookName, bookAuthor, bookYear, bookPublisher};
+
+}
+
+const {bookName, bookAuthor, bookYear, bookPublisher} = getBookInfo();
+
+
+function createBookObject(bkName, bkAu, bkYe, bkPub) {
+    const singularBook = new Book(`${bkAu}`, `${bkName}`, `${bkYe}`,`${bkPub}`);
+    return singularBook
+}
+
+const bookObject = createBookObject(bookName, bookAuthor, bookYear, bookPublisher);
+myLibrary.unshift(bookObject);
+console.log(myLibrary);
